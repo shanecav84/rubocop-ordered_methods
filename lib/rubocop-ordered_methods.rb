f678f31a898efbf552e5a612255278d0191806fc
Shane Cavanaugh <shane@shanecav.net>
2019-02-17 14:22:43 -0500

    Add default config

--- lib/rubocop-ordered_methods.rb
+++ lib/rubocop-ordered_methods.rb
@@ -1,3 +1,6 @@
 require 'rubocop'
+require_relative 'rubocop/ordered_methods'
 require_relative 'rubocop/cop/layout/ordered_methods'
 require_relative 'rubocop/cop/correctors/ordered_methods_corrector'
+
+RuboCop::OrderedMethods.inject_defaults!
---
+++ lib/rubocop/ordered_methods.rb
@@ -0,0 +1,15 @@
+module RuboCop
+  module OrderedMethods
+    PROJECT_ROOT = Pathname.new(__dir__).parent.parent.expand_path.freeze
+    CONFIG_DEFAULT = PROJECT_ROOT.join("config", "default.yml").freeze
+
+    def self.inject_defaults!
+      path = CONFIG_DEFAULT.to_s
+      hash = ConfigLoader.send(:load_yaml_configuration, path)
+      config = Config.new(hash, path)
+      puts "configuration from #{path}" if ConfigLoader.debug?
+      config = ConfigLoader.merge_with_default(config, path)
+      ConfigLoader.instance_variable_set(:@default_configuration, config)
+    end
+  end
+end
