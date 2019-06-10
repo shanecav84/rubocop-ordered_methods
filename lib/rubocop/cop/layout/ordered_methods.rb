9e628f10869b8a645ab2bfb383d16d43b18b8475
Shane Cavanaugh <shane@shanecav.net>
2019-06-10 08:21:43 -0400

    Clean up

--- lib/rubocop/cop/layout/ordered_methods.rb
+++ lib/rubocop/cop/layout/ordered_methods.rb
@@ -60,8 +60,10 @@ module RuboCop
             (node.send_type? && node.bare_access_modifier?)
         end
 
-        def consecutive_methods(ast)
-          filtered_and_grouped(ast).each do |method_group|
+        def consecutive_methods(nodes)
+          filtered = filter_relevant_nodes(nodes)
+          filtered_and_grouped = group_methods_by_access_modifier(filtered)
+          filtered_and_grouped.each do |method_group|
             method_group.each_cons(2) do |left_method, right_method|
               yield left_method, right_method
             end
@@ -77,10 +79,6 @@ module RuboCop
           end
         end
 
-        def filtered_and_grouped(ast)
-          group_methods_by_access_modifier(filter_relevant_nodes(ast))
-        end
-
         # Group methods by the access modifier block they are declared in.
         # Multiple blocks of the same modifier will have their methods grouped
         # separately; for example, the following would be separated into two
@@ -89,26 +87,19 @@ module RuboCop
         #   def a; end
         #   private
         #   def b; end
-        # rubocop:disable Metrics/MethodLength
         def group_methods_by_access_modifier(nodes)
           is_class_method_block = false
+
           nodes.each_with_object([[]]) do |node, grouped_methods|
             if access_modified?(node, is_class_method_block)
               grouped_methods << []
             end
-
-            is_class_method_block = true if node.defs_type?
-            is_class_method_block = false if node.def_type?
-
-            if node.send_type? && node.bare_access_modifier?
-              is_class_method_block = false
-              next
-            end
+            is_class_method_block = node.defs_type?
+            next if node.send_type? && node.bare_access_modifier?
 
             grouped_methods.last << node
           end
         end
-        # rubocop:enable Metrics/MethodLength
 
         def ordered?(left_method, right_method)
           (left_method.method_name <=> right_method.method_name) != 1
